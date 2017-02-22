1c55b42fd63d0bec5ad5ea3945e89e97cc5edf61
cjredmond <connor.redmond@gmail.com>
2017-02-22 15:39:38 -0500

    adding a form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import Projects from './components/Projects';
+import AddProject from './components/AddProject';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile Dev'
-        },
-        {
-          title: 'Ecommerce Shopping Cart',
-          category: 'Web Dev'
-        }
-      ]
+      projects: []
     }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Dev'
+      },
+      {
+        title: 'Ecommerce Shopping Cart',
+        category: 'Web Dev'
+      }
+    ]});
+  }
+
+  handleAddProject(project){
+    
   }
 
   render() {
     return (
       <div className="App">
-        My App
+       <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects} />
       </div>
     );
