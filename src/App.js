e52ebdb7dfe1044bbb72778a579954a189f2dfc6
cjredmond <connor.redmond@gmail.com>
2017-02-22 19:23:51 -0500

    installed uuid

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ class App extends Component {
   }
 
   handleAddProject(project){
-    
+    let projects = this.state.projects;
+    projects.push(project)
+    this.setState({projects:projects});
   }
 
   render() {
--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -30,7 +30,7 @@ class AddProject extends Component {
 
   render() {
     let categoryOptions = this.props.categories.map(category => {
-      return <option key={category} value="category">{category}</option>
+      return <option key={category} value={category}>{category}</option>
     });
 
     return (
@@ -47,6 +47,7 @@ class AddProject extends Component {
               {categoryOptions}
             </select>
           </div>
+          <br />
           <input type="submit" value="Submit" />
         </form>
       </div>
